99f1a636d8882844d6e3e209681e34bcdd277a23
Anthony <anthony.j.boyko@gmail.com>
2020-09-20 15:41:48 -0700

    completed add mthod for the calculator alongside testing

---
+++ Calculator/test/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+    @Test
+    void testAdd(){
+//        I will do two tests, 1 add a negative plus a postive and 2 add two positive numbers
+        int resultPositives = 15; // adding 5 + 10 should return 15
+        int resultMixed = 2; //adding postive 6 with negative 4 should return 2
+        Calculator test = new Calculator();
+        assertEquals(15, test.add(5,10));
+        assertEquals(2, test.add(6,-4));
+    }
+}
